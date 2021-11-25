4ae71a04cf1d993e285e3df687c1e2f7bfc143bd
leobert <leobert.l@hotmail.com>
2021-11-25 16:58:10 +0800

    tmp

--- src/net/pocketmagic/android/utils/CustomAdapter.java
+++ src/net/pocketmagic/android/utils/CustomAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Android Event Injector 
+ * Android Event Injector
  *
  * Copyright (c) 2013 by Radu Motisan , radu.motisan@gmail.com
  *
@@ -35,113 +35,107 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+
 import java.util.List;
 
 import net.pocketmagic.android.eventinjector.R;
 
 
+class CustomAdapterView extends RelativeLayout {
+    public CustomAdapterView(Context context, ListViewItem device) {
+        super(context);
+        // get/set ID
+        setId(device.getDeviceID());
+        // get image
+        int padding = 5;
+        int resImg = 0;
+        String szStatus = "";
+        if (device.getDeviceStatus()) {
+            resImg = R.drawable.list_open;
+            szStatus = "Open";
+        } else {
+            resImg = R.drawable.list;
+            szStatus = "Not open";
+        }
 
 
-class CustomAdapterView extends RelativeLayout {        	
-	public CustomAdapterView(Context context, ListViewItem device) 
-	{
-		super( context );
-		// get/set ID
-		setId(device.getDeviceID());
-		// get image
-		int padding = 5;
-		int resImg = 0;
-		String szStatus ="";
-		if (device.getDeviceStatus()) { 
-			resImg = R.drawable.list_open;
-			szStatus = "Open";
-		} else {
-			resImg = R.drawable.list;
-			szStatus = "Not open";
-		}		
-		
-		
- 		
-		// Configure holder layout
-		setBackgroundResource(R.drawable.panel_item);
-		//setOrientation(LinearLayout.HORIZONTAL);
-		setPadding(padding, 0, padding, 0);
-		//setGravity(Gravity.CENTER_VERTICAL);
-		
-		// LKEFT
-		ImageView ivLogo = new ImageView(context);
-		ivLogo.setId(100);
-		ivLogo.setImageDrawable(context.getResources().getDrawable(resImg));
-		//ivLogo.setBackgroundColor(Color.BLACK);
-		RelativeLayout.LayoutParams lp_ivLogo = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		lp_ivLogo.addRule(RelativeLayout.CENTER_VERTICAL);
-		lp_ivLogo.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-		addView(ivLogo, lp_ivLogo);
-		
-		
-		
-		LinearLayout panelV = new LinearLayout(context);
-		//panelV.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-		panelV.setOrientation(LinearLayout.VERTICAL);
-		panelV.setPadding(padding,0,0,0);
-		
-		RelativeLayout.LayoutParams lp_panel = new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-		lp_panel.addRule(RelativeLayout.CENTER_VERTICAL);
-		lp_panel.addRule(RelativeLayout.RIGHT_OF, ivLogo.getId());
-		//lp_panel.addRule(RelativeLayout.LEFT_OF, b.getId());
-		addView(panelV, lp_panel);
-		
-		// row1
-		TextView textName = new TextView( context );
-		textName.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f);
-		textName.setTypeface(Typeface.DEFAULT, Typeface.BOLD);
-		
-		textName.setText( device.getDeviceName());
-		panelV.addView(textName);
-		// row2
-		TextView textAddress = new TextView( context );
-		textAddress.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10f);
-		textAddress.setTypeface(Typeface.DEFAULT, Typeface.NORMAL);
-		textAddress.setText(device.getDevicePath());
-		panelV.addView(textAddress);    
-		// row3
-		TextView textStatus = new TextView( context );
-		textStatus.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10f);
-		textStatus.setTypeface(Typeface.DEFAULT, Typeface.BOLD);
-		textStatus.setText(szStatus);
-		panelV.addView(textStatus);
-		
-		
-		
-	}
+        // Configure holder layout
+        setBackgroundResource(R.drawable.panel_item);
+        //setOrientation(LinearLayout.HORIZONTAL);
+        setPadding(padding, 0, padding, 0);
+        //setGravity(Gravity.CENTER_VERTICAL);
+
+        // LKEFT
+        ImageView ivLogo = new ImageView(context);
+        ivLogo.setId(100);
+        ivLogo.setImageDrawable(context.getResources().getDrawable(resImg));
+        //ivLogo.setBackgroundColor(Color.BLACK);
+        RelativeLayout.LayoutParams lp_ivLogo = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        lp_ivLogo.addRule(RelativeLayout.CENTER_VERTICAL);
+        lp_ivLogo.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        addView(ivLogo, lp_ivLogo);
+
+
+        LinearLayout panelV = new LinearLayout(context);
+        //panelV.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        panelV.setOrientation(LinearLayout.VERTICAL);
+        panelV.setPadding(padding, 0, 0, 0);
+
+        RelativeLayout.LayoutParams lp_panel = new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+        lp_panel.addRule(RelativeLayout.CENTER_VERTICAL);
+        lp_panel.addRule(RelativeLayout.RIGHT_OF, ivLogo.getId());
+        //lp_panel.addRule(RelativeLayout.LEFT_OF, b.getId());
+        addView(panelV, lp_panel);
+
+        // row1
+        TextView textName = new TextView(context);
+        textName.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f);
+        textName.setTypeface(Typeface.DEFAULT, Typeface.BOLD);
+
+        textName.setText(device.getDeviceName());
+        panelV.addView(textName);
+        // row2
+        TextView textAddress = new TextView(context);
+        textAddress.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10f);
+        textAddress.setTypeface(Typeface.DEFAULT, Typeface.NORMAL);
+        textAddress.setText(device.getDevicePath());
+        panelV.addView(textAddress);
+        // row3
+        TextView textStatus = new TextView(context);
+        textStatus.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10f);
+        textStatus.setTypeface(Typeface.DEFAULT, Typeface.BOLD);
+        textStatus.setText(szStatus);
+        panelV.addView(textStatus);
+
+
+    }
 }
 
 public class CustomAdapter extends BaseAdapter {
 
-    private Context context;
-    private List<ListViewItem> deviceList;
+    private final Context context;
+    private final List<ListViewItem> deviceList;
 
-    public CustomAdapter(Context context, List<ListViewItem> deviceList ) { 
+    public CustomAdapter(Context context, List<ListViewItem> deviceList) {
         this.context = context;
         this.deviceList = deviceList;
     }
 
-    public int getCount() {                        
+    public int getCount() {
         return deviceList.size();
     }
 
-    public Object getItem(int position) {     
+    public Object getItem(int position) {
         return deviceList.get(position);
     }
 
-    public long getItemId(int position) {  
+    public long getItemId(int position) {
         return position;
     }
 
-    public View getView(int position, View convertView, ViewGroup parent) 
-    { 
-    	ListViewItem device = deviceList.get(position);
-        View v = new CustomAdapterView(this.context, device );
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ListViewItem device = deviceList.get(position);
+        View v = new CustomAdapterView(this.context, device);
         v.setOnClickListener((OnClickListener) context);
         return v;
     }
--- src/net/pocketmagic/android/utils/ListViewItem.java
+++ src/net/pocketmagic/android/utils/ListViewItem.java
@@ -1,5 +1,5 @@
 /*
- * Android Event Injector 
+ * Android Event Injector
  *
  * Copyright (c) 2013 by Radu Motisan , radu.motisan@gmail.com
  *
@@ -26,30 +26,50 @@ package net.pocketmagic.android.utils;
 
 
 public class ListViewItem {
-		   
-	    private String m_szDeviceName;
-	    private String m_szDevicePath;
-	    private boolean m_bConnected;
-	    private int m_nDeviceID, m_nConType;
 
-	    public ListViewItem( String deviceName, String DevicePath, boolean deviceStatus, int deviceID ) {
-	        m_szDeviceName = deviceName;
-	        m_szDevicePath = DevicePath;
-	        m_bConnected = deviceStatus;
-	        m_nDeviceID = deviceID;
-	      }
+    private String m_szDeviceName;
+    private String m_szDevicePath;
+    private boolean m_bConnected;
+    private int m_nDeviceID, m_nConType;
+
+    public ListViewItem(String deviceName, String DevicePath, boolean deviceStatus, int deviceID) {
+        m_szDeviceName = deviceName;
+        m_szDevicePath = DevicePath;
+        m_bConnected = deviceStatus;
+        m_nDeviceID = deviceID;
+    }
 
 
-	    public String getDeviceName() { return m_szDeviceName; }
-	    public void setDeviceName(String deviceName) { m_szDeviceName = deviceName;}
-	    
-	    public String getDevicePath() {return m_szDevicePath;}
-	    public void setDevicePath(String DevicePath) {m_szDevicePath = DevicePath;}
-	    
-	    
-	    public boolean getDeviceStatus() { return m_bConnected; }
-	    public void setDeviceStatus(boolean deviceStatus) { m_bConnected = deviceStatus;}
-	    
-	    public int getDeviceID() { return m_nDeviceID; }
-	    public void setDeviceID(int deviceID) { m_nDeviceID = deviceID;}
+    public String getDeviceName() {
+        return m_szDeviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        m_szDeviceName = deviceName;
+    }
+
+    public String getDevicePath() {
+        return m_szDevicePath;
+    }
+
+    public void setDevicePath(String DevicePath) {
+        m_szDevicePath = DevicePath;
+    }
+
+
+    public boolean getDeviceStatus() {
+        return m_bConnected;
+    }
+
+    public void setDeviceStatus(boolean deviceStatus) {
+        m_bConnected = deviceStatus;
+    }
+
+    public int getDeviceID() {
+        return m_nDeviceID;
+    }
+
+    public void setDeviceID(int deviceID) {
+        m_nDeviceID = deviceID;
+    }
 }
